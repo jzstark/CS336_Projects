36b650aec472a9ff27aa87ab2985bd48bb00c6fd
J. Roger Zhao <rho.ajax@gmail.com>
2025-08-01 19:46:48 +0200

    coding...

--- proj2-systems/cs336_systems/benchmark.py
+++ proj2-systems/cs336_systems/benchmark.py
@@ -179,11 +179,22 @@ Just too long...
 (1) Inference time
 
 Different passes shows very different timing results... 
-Warm-up: 891.3ms 
-1st 10 forward pass: 95ms 
-2nd 10 forward pass: 4.351s 
-3rd: 87.33ms 
-6th: 7.98s 
+
+Only forward: 
+
+Time	Total Time	Instances	Avg	Med	Min	Max	StdDev	Style	Range
+50.0%	200.301 s	1	200.301 s	200.301 s	200.301 s	200.301 s	0 ns	PushPop	:time_model_ntx
+30.4%	121.617 s	1	121.617 s	121.617 s	121.617 s	121.617 s	0 ns	PushPop	:Timing Step 2/10 with backward=False
+6.5%	26.167 s	1	26.167 s	26.167 s	26.167 s	26.167 s	0 ns	PushPop	:Timing Step 4/10 with backward=False
+5.3%	21.250 s	1	21.250 s	21.250 s	21.250 s	21.250 s	0 ns	PushPop	:Timing Step 6/10 with backward=False
+4.4%	17.439 s	1	17.439 s	17.439 s	17.439 s	17.439 s	0 ns	PushPop	:Timing Step 8/10 with backward=False
+2.7%	10.997 s	1	10.997 s	10.997 s	10.997 s	10.997 s	0 ns	PushPop	:Timing Step 10/10 with backward=False
+0.4%	1.505 s	1	1.505 s	1.505 s	1.505 s	1.505 s	0 ns	PushPop	:Warm-up
+0.1%	314.867 ms	1	314.867 ms	314.867 ms	314.867 ms	314.867 ms	0 ns	PushPop	:Timing Step 5/10 with backward=False
+0.1%	293.686 ms	1	293.686 ms	293.686 ms	293.686 ms	293.686 ms	0 ns	PushPop	:Timing Step 9/10 with backward=False
+0.1%	240.225 ms	1	240.225 ms	240.225 ms	240.225 ms	240.225 ms	0 ns	PushPop	:Timing Step 1/10 with backward=False
+0.1%	235.294 ms	1	235.294 ms	235.294 ms	235.294 ms	235.294 ms	0 ns	PushPop	:Timing Step 7/10 with backward=False
+0.0%	193.977 ms	1	193.977 ms	193.977 ms	193.977 ms	193.977 ms	0 ns	PushPop	:Timing Step 3/10 with backward=False
 
 Something is wrong with the timing... 
 
@@ -191,6 +202,21 @@ Total time:
 Forward test: 42.7s
 Backward test: 64.3s
 
-(2) 
+(2) CUDA Kernels
 
+ampere_sgemm_128x64_tn  66.5%   3795 instances
+Element-wise multiplication 10.3%  6510 instances
+Element-wise division 4%  540 instances
+Element-wise sigmoid activation 3.3% 540 instances 
+Element-wise multiply involving 3 tensors 3.3% 1080 instances
+Direct data copy between tensors 2.9% 340 instances
+
+
+(3) Element-wise kernels takes a lot of time besides GEMM
+
+(5)
+FLOPs(Softmax) / FLOPs(MatMuls) ≈ 2 / (4 x d) = O(1/d)
+For typical settings (d = 64), softmax FLOPs are ~1/32 of the attention MatMul FLOPs.
+
+Softmax should be relatively slow.
 """
